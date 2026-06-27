2299a17f25be13c135b8a0b521a4ddec676c104f
chanti111 <60770030+chanti111@users.noreply.github.com>
2026-06-27 16:22:38 +0530

    Create index.php

---
+++ webpage/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Smart Hospital</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>Smart Hospital Health Card System</h1>
+
+<div class="box">
+    <a href="register.php">New Patient Registration</a>
+    <a href="scan.php">Scan Patient Card</a>
+</div>
+
+</body>
+</html>
